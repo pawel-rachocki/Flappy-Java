e9176a51d16122314fb5ea9a9fc7a5a59b8b36ab
Pawel <pawel.logo@gmail.com>
2025-06-26 22:31:08 +0200

    Refactor with before added enum

--- src/App.java
+++ src/App.java
@@ -1,12 +1,13 @@
+import enums.BoardDimensions;
+
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int boardWidth = 360;
-        int boardHeight = 640;
+
 
         JFrame game = new JFrame("Flappy");
-        game.setSize(boardWidth,boardHeight);
+        game.setSize(BoardDimensions.BOARD_WIDTH.getValue(),BoardDimensions.BOARD_HEIGHT.getValue());
         game.setVisible(true);
         game.setLocationRelativeTo(null); // set game frame in the middle of the screen
         game.setResizable(false); // dont let user to resize the game frame
--- src/Flappy.java
+++ src/Flappy.java
@@ -1,2 +1,15 @@
-public class Flappy {
+import enums.BoardDimensions;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class Flappy extends JPanel{
+
+    Flappy(){
+        setPreferredSize(new Dimension(BoardDimensions.BOARD_WIDTH.getValue(), BoardDimensions.BOARD_HEIGHT.getValue()));
+    }
+
 }
