8707ec2424eea7bbea5065aa1185fcb44e8b1ae8
Pawel <pawel.logo@gmail.com>
2025-06-26 22:45:01 +0200

    Added image loading and paint component method

--- src/App.java
+++ src/App.java
@@ -1,17 +1,20 @@
 import enums.BoardDimensions;
-
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-
         JFrame game = new JFrame("Flappy");
         game.setSize(BoardDimensions.BOARD_WIDTH.getValue(),BoardDimensions.BOARD_HEIGHT.getValue());
-        game.setVisible(true);
         game.setLocationRelativeTo(null); // set game frame in the middle of the screen
         game.setResizable(false); // dont let user to resize the game frame
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Flappy flappy = new Flappy();
+        game.add(flappy);
+        game.pack(); // BoardDimensions.BOARD_WIDTH && BoardDimensions.BOARD_HEIGHT excluding title bar
+
+        game.setVisible(true);
 
     }
 }
--- src/Flappy.java
+++ src/Flappy.java
@@ -8,8 +8,32 @@ import javax.swing.*;
 
 public class Flappy extends JPanel{
 
+    // Assets/Images
+    Image backgroundImg;
+    Image playerImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+
     Flappy(){
         setPreferredSize(new Dimension(BoardDimensions.BOARD_WIDTH.getValue(), BoardDimensions.BOARD_HEIGHT.getValue()));
+        setBackground(Color.BLACK);
+
+        // load assets
+        backgroundImg = new ImageIcon(getClass().getResource("/assets/flappybirdbg.png")).getImage();
+        playerImg = new ImageIcon(getClass().getResource("/assets/flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("/assets/toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("/assets/bottompipe.png")).getImage();
     }
 
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+        draw(graphics);
+
+    }
+
+    public void draw(Graphics graphics){
+        // BackGround
+        graphics.drawImage(backgroundImg,0,0,BoardDimensions.BOARD_WIDTH.getValue(),BoardDimensions.BOARD_HEIGHT.getValue(),null);
+    }
 }
