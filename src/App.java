90b775b19868f60ccedcf5f58de12b1d14205430
Pawel <pawel.logo@gmail.com>
2025-06-26 22:05:21 +0200

    Added Game frame App class + assets

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("hw");
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame game = new JFrame("Flappy");
+        game.setSize(boardWidth,boardHeight);
+        game.setVisible(true);
+        game.setLocationRelativeTo(null); // set game frame in the middle of the screen
+        game.setResizable(false); // dont let user to resize the game frame
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
